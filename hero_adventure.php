b345d4ace1ebcda910d180d9ac0eb699d8c32e40
seven70 <roza.pishi@yahoo.com>
2012-03-18 19:10:32 +0430

    Update hero_adventure.php

--- hero_adventure.php
+++ hero_adventure.php
@@ -1,1 +1,19 @@
-<?php header("Location: dorf1.php"); ?>
+<?php
+include("GameEngine/Village.php");
+
+$start = $generator->pageLoadTimeStart();
+
+if(isset($_GET['newdid'])) {
+        $_SESSION['wid'] = $_GET['newdid'];
+        header("Location: ".$_SERVER['PHP_SELF']);
+}
+else {
+        $building->procBuild($_GET);
+}
+
+if(ZRAVIANX4 == true){
+        include("Templates/Travian/4/hero_adv.tpl");
+} else {
+        include("Templates/Travian/3.6/hero_adv.tpl");
+}
+?>
